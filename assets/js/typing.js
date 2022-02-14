e4ca51dc89c7e496fb2dfb0dcd7426d7cef661e5
pyaethuta <97032450+calciferp88@users.noreply.github.com>
2022-02-14 12:21:19 +0630

    Feb 2022 Updated

--- assets/js/typing.js
+++ assets/js/typing.js
@@ -1,7 +1,7 @@
 const typedTextSpan = document.querySelector(".typed-text");
 const cursorSpan = document.querySelector(".cursor");
 
-const textArray = ["Web Developer", "UI/UX designer", "Content Manager", "Aquarium Keeper"];
+const textArray = ["Fullstack Web Developer", "UI/UX designer", "Project Manager"];
 const typingDelay = 200;
 const erasingDelay = 100;
 const newTextDelay = 2000; // Delay between current and next text
